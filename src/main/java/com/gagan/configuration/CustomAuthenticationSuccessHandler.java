4fa9fc4cf80acae681b949817af9e2ff1229252e
gagan <gagan.chhabra1990@gmail.com>
2018-03-14 18:42:40 +0530

    new commit

--- src/main/java/com/gagan/configuration/CustomAuthenticationSuccessHandler.java
+++ src/main/java/com/gagan/configuration/CustomAuthenticationSuccessHandler.java
@@ -1,7 +1,33 @@
 package com.gagan.configuration;
 
-/**
- * Created by pc on 1/16/2018.
- */
-public class CustomAuthenticationSuccessHandler {
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.DefaultRedirectStrategy;
+import org.springframework.security.web.RedirectStrategy;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class CustomAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
+
+    private RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+        handle(request, response, authentication);
+    }
+
+    protected void handle(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
+        System.out.println("nnnnnnnnnnnnnnnn");
+        String targetUrl = "home";
+        redirectStrategy.sendRedirect(request, response, targetUrl);
+    }
 }
--- src/main/java/com/gagan/configuration/SecurityConfig.java
+++ src/main/java/com/gagan/configuration/SecurityConfig.java
@@ -1,7 +1,48 @@
 package com.gagan.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
 /**
- * Created by pc on 1/16/2018.
+ * @author Dinesh.Rajput
+ *
  */
-public class SecurityConfig {
+/* To enable Spring Security integration with Spring MVC add the @EnableWebSecurity annotation to your configuration.*/
+@Configuration
+@EnableWebSecurity
+@ComponentScan({"com.gagan.configuration"})
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    //Override Default configuration in WebSecurityConfigurerAdapter for custom login form and authorize requests
+    //We specified multiple URL patterns that any user can access like "/login/".
+    //Any URL that starts with "/admin/" will be restricted to users who have the role "ROLE_ADMIN".
+    //Any URL that has not already been matched on only requires that the user be authenticated
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/resources/**", "/login").permitAll()
+                .antMatchers("/admin/**").hasRole("ADMIN")
+                .antMatchers("/db/**").access("hasRole('ADMIN') and hasRole('DBA')")
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                    .successHandler(new CustomAuthenticationSuccessHandler());
+
+    }
+    //In memory authentication java configuration
+    //Not web-specific
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication() //Adds a UserDetailsManagerConfigurer
+                //login, password and supported role
+                .withUser("user").password("password").roles("USER").and()
+                .withUser("admin").password("dinesh").roles("ADMIN");
+    }
 }
